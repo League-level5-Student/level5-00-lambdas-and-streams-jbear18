037cfb699abd858da27e5b913a04985405f8c105
jbear18 <shehu01@hotmail.com>
2021-01-28 17:00:02 -0800

    lambda methods ip

--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -3,32 +3,36 @@ package _01_Lambda_Methods;
 public class LambdaMethods {
 	public static void main(String[] args) {
 		// 1. Look at the SpecialPrinter function interface.
-	
+
 		// Here is an example of calling the printCustomMessage method with a lambda.
 		// This prints the passed in String 10 times.
-		printCustomMessage((s)->{
-			for(int i = 0; i < 10; i++) {
+		printCustomMessage((s) -> {
+			for (int i = 0; i < 10; i++) {
 				System.out.println(s);
 			}
 		}, "repeat");
-		
-		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		printCustomMessage((s)->{
-			String str="";
-			for(int j= s.length(); j>0; j--){
-		            str+= s.substring(j-1, j);
+
+		// 2. Call the printCustomMessage method using a lambda so that the String
+		// prints backwards.
+		printCustomMessage((s) -> {
+			String str = "";
+			for (int j = s.length(); j > 0; j--) {
+				str += s.substring(j - 1, j);
 			}
-		}, str);	
-		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
-		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
-		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
-	
-	
+			System.out.println(str);
+		}, "Preston is weird!");
+
+	}
+	// 3. Call the printCustonMessage method using a lambda so that the String
+	// prints with a mix between upper an lower case characters.
+
+	// 4. Call the printCustonMessage method using a lambda so that the String
+	// prints with a period in between each character.
+
+	// 5. Call the printCustonMessage method using a lambda so that the String
+	// prints without any vowels.
+
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
 		sp.printSpecial(value);
 	}
 }
-	
